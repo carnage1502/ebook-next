02bac3ddf4cb930cc9ef5f884c1a6d57d9a91e3b
carnage1502 <shivamsanjay152@gmail.com>
2024-03-25 14:50:09 +0530

    complete sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
+"use client";
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { books } from "@/constants/mockData";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -6,7 +10,31 @@ export default function Home() {
       <div>
         <Header />
 
-        <div></div>
+        <div className="flex justify-between items-start">
+          <section className="w-[30%]">
+            <Sidebar />
+          </section>
+
+          <div>
+            <h1 className="font-bold text-4xl">All Books</h1>
+            <ul>
+              {books.map((book, i) => (
+                <motion.li
+                  key={i}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                  transition={{ type: "spring", damping: 50, mass: 0.75 }}
+                  initial={{ opacity: 0, x: 200 * (i + 1) }}
+                  animate={{ opacity: 1, x: 0 }}
+                >
+                  <a href={`/book/${book.id}`}>
+                    <p>{book.title}</p>
+                  </a>
+                </motion.li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { motion } from "framer-motion";
+import {
+  FaHome,
+  FaEnvelope,
+  FaInfoCircle,
+  FaCog,
+  FaStar,
+  FaEye,
+} from "react-icons/fa";
+
+const Sidebar = () => {
+  const MenuList = [
+    {
+      title: "Home",
+      icon: <FaHome className="mr-2 inline-block" />,
+    },
+    {
+      title: "Contact",
+      icon: <FaEnvelope className="mr-2 inline-block" />,
+    },
+    {
+      title: "AboutUs",
+      icon: <FaInfoCircle className="mr-2 inline-block" />,
+    },
+    {
+      title: "Settings",
+      icon: <FaCog className="mr-2 inline-block" />,
+    },
+    {
+      title: "Contact",
+      icon: <FaHome className="mr-2 inline-block" />,
+    },
+    {
+      title: "RateUs",
+      icon: <FaStar className="mr-2 inline-block" />,
+    },
+    {
+      title: "Change Password",
+      icon: <FaEye className="mr-2 inline-block" />,
+    },
+    {
+      title: "Settings",
+      icon: <FaCog className="mr-2 inline-block" />,
+    },
+  ];
+  return (
+    <>
+      {MenuList.map((list, i) => (
+        <motion.div
+          key={i}
+          className=""
+          transition={{ type: "spring", damping: 22, mass: 0.99 }}
+          initial={{ opacity: 0, x: -2000 * (i + 1) }}
+          animate={{ opacity: 1, x: 0 }}
+        >
+          <ul className="list-none p-0 w-[200px]">
+            {
+              <motion.li
+                className="mb-4 text-xl p-1.5 cursor-pointer rounded-[10px] shadow-[0px_0px_10px_rgba(0,0,0,0.1)]"
+                whileHover={{ scale: 1.1 }}
+              >
+                <motion.span
+                  transition={{ type: "spring", damping: 30, mass: 0.99 }}
+                  initial={{ opacity: 0, x: -10000 * (i + 1) }}
+                  animate={{ opacity: 1, x: 0 }}
+                  style={{
+                    color: "#000",
+                    textDecoration: "none",
+                    fontSize: "14px",
+                    fontWeight: "600",
+                    marginLeft: "10px",
+                    lineHeight: 2,
+                  }}
+                >
+                  {list.icon}
+                  {list.title}
+                </motion.span>
+              </motion.li>
+            }
+          </ul>
+        </motion.div>
+      ))}
+    </>
+  );
+};
+
+export default Sidebar;
